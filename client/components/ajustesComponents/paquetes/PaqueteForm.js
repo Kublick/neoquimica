a175e16a2171337dfca50040cfc6bd140bd99edf
Max Blancarte <blancarte@gmail.com>
2021-07-08 12:54:29 -0700

    add paquetes

--- client/components/ajustesComponents/paquetes/PaqueteForm.js
+++ client/components/ajustesComponents/paquetes/PaqueteForm.js
@@ -1,7 +1,197 @@
-const PaqueteForm = () => {
+import { useForm, Controller } from "react-hook-form";
+import Input from "@material-tailwind/react/Input";
+import CardHeader from "@material-tailwind/react/CardHeader";
+import Card from "@material-tailwind/react/Card";
+import CardBody from "@material-tailwind/react/CardBody";
+import { useEffect, useState } from "react";
+import Button from "@material-tailwind/react/Button";
+import Textarea from "@material-tailwind/react/Textarea";
+import SelectDrag from "./SelectDrag";
+
+const PaqueteForm = ({ add, update, results, edit, setShowModal }) => {
+	const [list, setList] = useState([]);
+	const [step, setStep] = useState(false);
+	const [formData, setFormData] = useState("");
+	const [fieldData, setFieldData] = useState([]);
+
+	const {
+		handleSubmit,
+		reset,
+		control,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			abreviatura: "",
+			descripcion: "",
+			indicaciones: "",
+			notasInternas: "",
+		},
+		//resolver: yupResolver(schema),
+		mode: "onBlur",
+	});
+
+	useEffect(() => {
+		if (!results) {
+			return <p>Loading...</p>;
+		}
+
+		const pruebas = results[0].data;
+		const perfiles = results[1].data;
+
+		let content = [];
+		perfiles.map((p) =>
+			content.push({
+				value: p._id,
+				label: `Perfil > ${p.descripcion} - ${p.metodo}`,
+			})
+		);
+		pruebas.map((p) =>
+			content.push({
+				value: p._id,
+				label: `Prueba > ${p.titulo} - ${p.metodo}`,
+			})
+		);
+		setFieldData(content);
+	}, []);
+
+	const resetForm = () => {
+		reset();
+	};
+
+	const onSubmit = (data) => {
+		setFormData(data);
+		setStep(true);
+	};
+
+	const onFinalSubmit = (data) => {
+		let finalData = { ...formData, bundle: data };
+		if (edit) {
+			finalData = { ...finalData, id: selectPaquete._id };
+			//dispatch(editPaquete(finalData));
+		} else {
+			finalData = { ...finalData, precio: 0 };
+			add.mutateAsync(finalData);
+			console.log("submitted");
+			//dispatch(addPaquete(finalData));
+		}
+	};
+
 	return (
-		<div>
-			<h1>Formulario</h1>
+		<div className="mt-20">
+			<Card>
+				<CardHeader>
+					<h2 className="text-2xl text-white">Registro Paquetes</h2>
+				</CardHeader>
+				<CardBody>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<div>
+							<Controller
+								name="abreviatura"
+								control={control}
+								defaultValue=""
+								render={({ field: { ref, ...field } }) => (
+									<Input
+										type="text"
+										color="lightBlue"
+										size="regular"
+										outline={false}
+										placeholder="Abreviatura"
+										error={errors.abreviatura?.message}
+										{...field}
+									/>
+								)}
+							/>
+						</div>
+						<div className="mt-2">
+							<Controller
+								name="descripcion"
+								control={control}
+								defaultValue=""
+								render={({ field: { ref, ...field } }) => (
+									<Input
+										type="text"
+										color="lightBlue"
+										size="regular"
+										outline={false}
+										placeholder="Descripcion"
+										error={errors.descripcion?.message}
+										{...field}
+									/>
+								)}
+							/>
+						</div>
+						<div className="mt-8">
+							<Controller
+								name="indicaciones"
+								control={control}
+								render={({ field: { ref, ...field } }) => (
+									<Textarea
+										color="lightBlue"
+										size="regular"
+										outline={false}
+										placeholder="Indicaciones"
+										{...field}
+									/>
+								)}
+							/>
+						</div>
+						<div className="mt-8">
+							<Controller
+								name="notasInternas"
+								control={control}
+								render={({ field: { ref, ...field } }) => (
+									<Textarea
+										color="lightBlue"
+										size="regular"
+										outline={false}
+										placeholder="Notas Internas"
+										{...field}
+									/>
+								)}
+							/>
+						</div>
+						{!step ? (
+							<div className="flex justify-end mt-2">
+								<Button
+									color="red"
+									buttonType="filled"
+									size="regular"
+									rounded={false}
+									iconOnly={false}
+									ripple="light"
+									className="mx-4"
+									onClick={() => {
+										resetForm();
+										setShowModal(false);
+									}}
+									type="button"
+								>
+									Cancelar
+								</Button>
+								<Button
+									color="lightBlue"
+									buttonType="filled"
+									size="regular"
+									rounded={false}
+									iconOnly={false}
+									ripple="light"
+									className="mx-4"
+								>
+									Siguiente
+								</Button>
+							</div>
+						) : null}
+					</form>
+				</CardBody>
+				{step ? (
+					<SelectDrag
+						onFinalSubmit={onFinalSubmit}
+						data={fieldData}
+						resetForm={resetForm}
+						list={list}
+					/>
+				) : null}
+			</Card>
 		</div>
 	);
 };
--- client/pages/ajustes/paquetes.js
+++ client/pages/ajustes/paquetes.js
@@ -1,15 +1,58 @@
 import PaqueteBoard from "../../components/ajustesComponents/paquetes/PaqueteBoard";
 import PaqueteForm from "../../components/ajustesComponents/paquetes/PaqueteForm";
 import Layout from "../../components/layout/Layout";
+import { useMutation, useQueryClient, useQueries } from "react-query";
+import {
+	addPaquete,
+	getPerfiles,
+	getPruebas,
+	updatePaquete,
+} from "../../components/api/ajustesApi";
+import { useState } from "react";
 
 const Paquetes = () => {
+	const [showModal, setShowModal] = useState(false);
+	const [editData, setEditData] = useState("");
+	const [titulo, setTitulo] = useState("");
+
+	const queryClient = useQueryClient();
+	const results = useQueries([
+		{ queryKey: ["pruebas"], queryFn: getPruebas },
+		{ queryKey: ["perfil"], queryFn: getPerfiles },
+	]);
+
+	const add = useMutation(addPaquete, {
+		onSuccess: () => {
+			queryClient.invalidateQueries("paquete");
+		},
+	});
+
+	const update = useMutation(updatePaquete, {
+		onSuccess: () => {
+			queryClient.invalidateQueries("paquete");
+		},
+	});
+
+	if (!results) {
+		return <p>Cargando...</p>;
+	}
+
 	return (
 		<Layout>
 			<div className="h-20 px-3 bg-light-blue-500 md:px-8" />
 			<div className="px-3 -mt-24 md:px-8">
 				<div className="xl:col-start-1 xl:col-end-4 mb-14">
-					<PaqueteBoard />
-					<PaqueteForm />
+					{!showModal ? (
+						<PaqueteBoard setTitulo={setTitulo} setShowModal={setShowModal} />
+					) : (
+						<PaqueteForm
+							add={add}
+							update={update}
+							results={results}
+							edit={editData}
+							setShowModal={setShowModal}
+						/>
+					)}
 				</div>
 			</div>
 		</Layout>
